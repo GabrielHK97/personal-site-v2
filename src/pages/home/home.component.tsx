bca20272a60b80d6130b5280b84bfd591dcc7937
gabriel.kubota <gabriel.kubota@umobium.com.br>
2024-03-12 21:13:12 -0300

    adjustments

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -3,10 +3,9 @@ import "./home.style.css";
 export function Home() {
   return (
     <div className="home">
-      <div className="home-menu">menu</div>
-      <div className="frame-1 home-main">teste</div>
-      <div className="frame-1 home-main-image">
-      </div>
+      <div className="home-menu faded-box">menu</div>
+      <div className="home-main faded-box">main</div>
+      <div className="home-sections faded-box">sections</div>
     </div>
   );
 }
